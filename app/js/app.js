379c9cf931a3d7d5383b593a3713704860537187
Bruno Buck <b.brigagao@gmail.com>
2018-04-06 21:05:06 -0300

    - Listing all messages; - Validation added; - Timestamp (date & time) for all messages, added; - Applied bootstrap (make it pretty).

--- app/js/app.js
+++ app/js/app.js
@@ -1,22 +1,30 @@
 angular.module('myApp', ['io.service']).
 
-run(function (io) {
-  io.init({
-    ioServer: 'http://localhost:3696',
-  });
-}).
-
-controller('MainController', function ($scope, io) {
-
-  $scope.send = function () {
-    io.emit({
-      message: $scope.message
+  run(function (io) {
+    io.init({
+      ioServer: 'http://localhost:3696',
     });
-    $scope.message = null;
-  }
+  }).
 
-  io.watch('message', function (data) {
-    $scope.lastMessage = data.message;
-    $scope.$apply();
+  controller('MainController', function ($scope, io) {
+
+    $scope.msglog = [];
+    /* $scope.$watch('message', function () {
+      $scope.disabled = (($scope.message !== null) && (!angular.isUndefined($scope.message))) ? false : true;
+    }); */
+
+    $scope.send = function () {
+      $scope.text.timestamp = new Date().toLocaleString();
+      io.emit({
+        message: $scope.text.message,
+        timestamp: $scope.text.timestamp
+      });
+      $scope.msglog.push(angular.copy($scope.text));
+      delete $scope.text.message;
+      delete $scope.text.timestamp;
+    }
+
+    io.watch('message', function (data) {
+      $scope.$apply();
+    });
   });
-});
